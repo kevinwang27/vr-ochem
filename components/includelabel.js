268e0a636974a1830ada6a479c8b1dc1e997fb92
Kevin <kevinwang27@berkeley.edu>
2018-04-16 20:40:55 -0700

    include label and atom work

--- components/includelabel.js
+++ components/includelabel.js
@@ -7,9 +7,14 @@ AFRAME.registerComponent('includelabel', {
         var data = this.data;
         var el = this.el;
 
+        var scene = document.querySelector('a-scene');
+
         var textEntity = document.createElement('a-text');
+        console.log(data.text);
+        
         textEntity.setAttribute('value', data.text);
         textEntity.setAttribute('position', '0 0 ' + el.getAttribute('geometry').radius);
+        textEntity.setAttribute('color', '#000');
         el.appendChild(textEntity);
     }
 });
