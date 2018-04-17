90d8515391e18d661c07103793997fd7320e3c5d
Kevin <kevinwang27@berkeley.edu>
2018-04-17 11:05:53 -0700

    MATH

--- components/shellatoms.js
+++ components/shellatoms.js
@@ -4,17 +4,28 @@ AFRAME.registerComponent('shellatoms', {
         var el = this.el;
         var scene = document.querySelector('a-scene');
 
-        var bondPos = el.getAttribute('position');
-        var bondRot = el.getAttribute('rotation');
-        var bondLength = el.getAttribute('geometry').height;
+        function calcAtomCoord(direction) {
+            var bondPos = el.getAttribute('position');
+            var bondRot = el.getAttribute('rotation');
+            var bondLength = el.getAttribute('geometry').height;
+            
+            if (direction == 1) {
+                return bondPos.x + bondLength / 2 * Math.sin(bondRot.x * Math.PI / 180.0);
+            } else if (direction == 2) {
+                return bondPos.y + bondLength / 2 * Math.cos(bondRot.y * Math.PI / 180.0);
+            } else {
+                return bondPos.z + bondLength / 2 * Math.sin(bondRot.z * Math.PI / 180.0);
+            }
+        }
 
-        var atomPosX = [bondPos.x];
-        var atomPosY = [bondPos.y];
-        var atomPosZ = [bondPos.z];
+        var atomPosX = [calcAtomCoord(1)];
+        var atomPosY = [calcAtomCoord(2)];
+        var atomPosZ = [calcAtomCoord(3)];
 
         for (var i = 0; i < atomPosX.length; i++) {
             var newEntity = document.createElement('a-atom');
-            newEntity.object3D.position.set(atomPosX[i], atomPosY[i], atomPosZ[i]);
+            newEntity.setAttribute('position', {x: atomPosX[i], y: atomPosY[i], z: atomPosZ[i]});
+            newEntity.setAttribute('opacity', '0.3');
             scene.appendChild(newEntity);
         }
     }
